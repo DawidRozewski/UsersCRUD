9674f996fb26c93338c4e90b4c9e49d373e89c85
DawidRozewski <dawid.rrozewski@gmail.com>
2021-08-04 22:41:33 +0200

    Added User class

---
+++ src/main/java/pl/coderslab/user/User.java
@@ -0,0 +1,40 @@
+package pl.coderslab.user;
+
+public class User {
+    private int id;
+    private String email;
+    private String username;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
