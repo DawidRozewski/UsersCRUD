0fa626e1d345224027a4ca53dccbc3ce164fff24
DawidRozewski <dawid.rrozewski@gmail.com>
2021-08-05 19:58:48 +0200

    Added BCrypt class to hash passwords

--- src/main/java/pl/coderslab/users/UserAdd.java
+++ src/main/java/pl/coderslab/users/UserAdd.java
@@ -9,6 +9,11 @@ import java.io.IOException;
 public class UserAdd extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserDAO userDAO = new UserDAO();
+
+
+
+
 
     }
 
