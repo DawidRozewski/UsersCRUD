3189561ad0b9cca4f52ab5abdeb37167d9d3efa4
DawidRozewski <dawid.rrozewski@gmail.com>
2021-08-05 18:10:10 +0200

    Added EncodingFilter Servlet

---
+++ src/main/java/pl/coderslab/UserDAO.java
@@ -0,0 +1,110 @@
+package pl.coderslab;
+
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+public class UserDAO {
+
+    private static final String CREATE_USER_QUERY = "INSERT INTO users(email, username, password) VALUES (?, ?, ?)";
+    private static final String READ_USER = "SELECT * FROM users WHERE id = ?";
+    private static final String UPDATE_USER = "UPDATE users SET email = ?, username = ?, password = ? WHERE id = ?";
+    private static final String DELETE_USER = "DELETE  FROM users WHERE id = ?";
+    private static final String SELECT_ALL_USERS = "SELECT * FROM users";
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(CREATE_USER_QUERY, PreparedStatement.RETURN_GENERATED_KEYS);
+            preStmt.setString(1, user.getEmail());
+            preStmt.setString(2, user.getUsername());
+            preStmt.setString(3, hashPassword(user.getPassword()));
+            preStmt.executeUpdate();
+            ResultSet rs = preStmt.getGeneratedKeys();
+            if (rs.next()) {
+                user.setId(rs.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public User read(int userID) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(READ_USER);
+            preStmt.setInt(1, userID);
+            ResultSet rs = preStmt.executeQuery();
+            if (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setEmail(rs.getString("email"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void update(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(UPDATE_USER);
+            preStmt.setString(1, user.getEmail());
+            preStmt.setString(2, user.getUsername());
+            preStmt.setString(3, hashPassword(user.getPassword()));
+            preStmt.setInt(4, user.getId());
+            preStmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User[] findAll() {
+        try (Connection conn = DbUtil.getConnection()) {
+            User[] users = new User[0];
+            PreparedStatement preparedStatement = conn.prepareStatement(SELECT_ALL_USERS);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setEmail("email");
+                user.setUsername("username");
+                users = addToArray(user, users);
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void delete(int userId) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement prStmt = conn.prepareStatement(DELETE_USER);
+            prStmt.setInt(1, userId);
+            prStmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1);
+        tmpUsers[users.length] = u;
+        return tmpUsers;
+    }
+}
