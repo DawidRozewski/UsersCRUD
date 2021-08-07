392b512a0b4f59b69b860222f429b23b696069fa
DawidRozewski <dawid.rrozewski@gmail.com>
2021-08-07 08:01:49 +0200

    Added method to delete user

--- src/main/java/pl/coderslab/users/UserDelete.java
+++ src/main/java/pl/coderslab/users/UserDelete.java
@@ -9,11 +9,19 @@ import java.io.IOException;
 public class UserDelete extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String id = request.getParameter("id");
+        UserDAO userDAO = new UserDAO();
+        User readUser = userDAO.read(Integer.parseInt(id));
+        request.setAttribute("user", readUser);
+          getServletContext().getRequestDispatcher("/users/deleteUser.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            int id = Integer.parseInt(request.getParameter("id"));
+            UserDAO userDAO = new UserDAO();
+            userDAO.delete(id);
+            response.sendRedirect(request.getContextPath() + "/user/list");
 
     }
 }
